67683117fc5dc8465389bec002b72d0f37968da7
tomer-leptop <tomerico007@gmail.com>
2025-04-04 16:05:59 +0300

    optimaze

--- bandaging-machine.ino
+++ bandaging-machine.ino
@@ -20,18 +20,11 @@ void setup() {
   digitalWrite(MOTOR2_ENABLE_PIN, LOW);
 
 
-  motor1.setMaxSpeed(1000);
-  motor1.setAcceleration(1000);
-  motor1.moveTo(STEPS_PER_REV);
-
-  motor2.setMaxSpeed(1000);
-  motor2.setAcceleration(1000);
-  motor2.moveTo(STEPS_PER_REV);
+  configur;
 }
 
 void loop() {
-  motor1.run();
-  motor2.run();
+  start;
 
   if (motor1.distanceToGo() == 0) {
     motor1.moveTo(-motor1.currentPosition());
@@ -39,4 +32,19 @@ void loop() {
   if (motor2.distanceToGo() == 0) {
     motor2.moveTo(-motor2.currentPosition());
   }
+}
+
+void start() {
+  motor1.run();
+  motor2.run();
+}
+
+void configur() {
+  motor1.setMaxSpeed(1000);
+  motor1.setAcceleration(1000);
+  motor1.moveTo(STEPS_PER_REV);
+
+  motor2.setMaxSpeed(1000);
+  motor2.setAcceleration(1000);
+  motor2.moveTo(STEPS_PER_REV);
 }
